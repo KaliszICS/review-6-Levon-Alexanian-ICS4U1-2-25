422ea71ad3e6978915d68e5993f9b0cac895e467
Levon-Alexanian-ICS4U1-2-25 <levon.alexanian@student.tdsb.on.ca>
2025-03-25 00:34:23 +0000

    update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,53 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static int sum(int a, int b) {
+		return a + b;
 
 	}
 
+
+	public static int difference(int a, int b) {
+		return a - b;
+
+	}
+
+	
+	public static double product(double a, double b) {
+		return a * b;
+
+	}
+
+	
+	public static String removeFirst(String word) {
+		return word.substring(1);
+
+	}
+
+	
+	public static int combinedLength(String word, String word1) {
+		return word.length() + word1.length();
+
+	}
+
+	
+	public static boolean isEven(int a) {
+		return a % 2 == 0;
+
+	}
+
+	
+	public static boolean isOdd(int a) {
+		return a % 2 != 0;
+
+	}
+
+	
+	public static boolean isPositive(int a) {
+		return a > 0;
+
+	}
+	public static boolean isNegative(int a) {
+		return a < 0;
+
+	}
 }
